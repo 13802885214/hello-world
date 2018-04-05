be0189d3de9fed3e1748fcd383e1e1d0f6c5c172
weikai <137142588@qq.com>
2018-04-05 22:54:47 +0800

    更改了什么

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by weikai on 2018/4/5.
+//  Copyright © 2018年 weikai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by weikai on 2018/4/5.
+//  Copyright © 2018年 weikai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
